f00cfe5c94a32b40af959875542b0ea3f6ccd383
Gokul Krishna jayan <91828907+Gokulkrishnajayan@users.noreply.github.com>
2023-09-20 12:24:03 +0530

    Program to add two number given by the user

--- add_two_num.py
+++ add_two_num.py
@@ -2,4 +2,4 @@ num1 = input('Enter first number: ')
 num2 = input('Enter second number: ')
 sum = int(num1) + int(num2)
 
-print('The sum of {} and {} is {}'.format(num1, num2, sum))
+print("The sum of "+str(num1)+" and "+str(num2)+" is "+str(sum))
