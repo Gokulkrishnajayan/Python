5195fcd785ea5a5cd1314c30f8144712f56f791e
Gokul Krishna jayan <91828907+Gokulkrishnajayan@users.noreply.github.com>
2023-09-20 23:45:31 +0530

    Program to print in python

---
+++ print.py
@@ -0,0 +1,56 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "name": "print_gokuldragon.py",
+      "authorship_tag": "ABX9TyPmLbFnYUcNxh3+g7MEcpgY",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Gokulkrishnajayan/Python/blob/main/print.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "print(\"gokul Dragon\")\n",
+        "\n",
+        "\n"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "QH9LpPoCtaQ3",
+        "outputId": "7054db9c-bc9b-4da8-e075-5507b4dcc0f5"
+      },
+      "execution_count": 2,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "gokul\n"
+          ]
+        }
+      ]
+    }
+  ]
+}
