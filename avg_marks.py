c53b9988f3de5ee6e0db3de834ae7cf8e5243c1f
Gokul Krishna jayan <91828907+Gokulkrishnajayan@users.noreply.github.com>
2023-10-04 11:40:07 +0530

    Program to accept the marks of five subjects and calculate the average marks

---
+++ avg_marks.py
@@ -0,0 +1,7 @@
+mark1 = int(input("Enter first subject marks: "))
+mark2 = int(input("Enter second subject marks: "))
+mark3 = int(input("Enter third subject marks: "))
+mark4 = int(input("Enter fourth subject marks: "))
+mark5 = int(input("Enter fifth subject marks: "))
+avg = (mark1 + mark2+ mark3+ mark4 + mark5) / 5
+print("Average Marks =", avg)
