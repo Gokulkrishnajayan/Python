e8c4a84408bee944490c7be8bb10cb5be74602d4
Gokul Krishna jayan <91828907+Gokulkrishnajayan@users.noreply.github.com>
2023-11-22 11:29:36 +0530

    Program to use try and else

---
+++ try_else.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the number"))
+b=int(input("Enter the number"))
+try:
+    c=a/b
+    print("Result",c)
+except:
+       print("can't divide by zero")
+else:
+     print("hi")
+        
