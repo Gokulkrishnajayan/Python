7c138b6c5ac57835c341093b3165b1671d710c0b
Gokul Krishna jayan <91828907+Gokulkrishnajayan@users.noreply.github.com>
2023-11-29 12:02:34 +0530

    Update first_n_line_file.py

--- first_n_line_file.py
+++ first_n_line_file.py
@@ -1,5 +1,6 @@
+file_path = 'gokul.txt'
 n = 5
-file=open('gokul.txt', 'r')
+file=open(file_path, 'r')
 print("First",n,"lines of",file_path,":\n")
 for line in range(n):
     print(file.readline().strip())
