0327f5017151e67732374fb980e9a126a5520589
Gokul Krishna jayan <91828907+Gokulkrishnajayan@users.noreply.github.com>
2023-11-29 12:25:45 +0530

    Program to find square root,factorial and pi

---
+++ sqrt_factorial_pi.py
@@ -0,0 +1,7 @@
+import math
+
+number = int(input("Enter a number: "))
+
+print("Square root of",user_input,":",math.sqrt(number))
+print("Factorial of",user_input,":",math.factorial(number))
+print("Value of pi:",math.pi)
